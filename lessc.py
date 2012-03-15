b09e9b9f88700bf713e1ec074bf22f9eff4f0d53
Sergi de Pablos <sergi@ulabox.com>
2012-03-15 16:05:40 +0100

    First commit

---
+++ lessc.py
@@ -0,0 +1,39 @@
+import sublime
+import sublime_plugin
+import os
+import subprocess
+from lessc_opts import lessc_opts
+
+
+class CompileLessOnSave(sublime_plugin.EventListener):
+
+    def on_post_save(self, view):
+
+        if not view.file_name().endswith('.less'):
+            return
+
+        folder_name, file_name = os.path.split(view.file_name())
+        args = []
+
+        opts = None
+        for path in lessc_opts:
+            if folder_name in path and len(folder_name) == (len(path) - 1):
+                opts = lessc_opts[path]
+                opts['less_path'] = path
+
+        if opts is None:
+            view.set_status('less', 'File saved but no LESS configuration found for ' + folder_name)
+            return
+
+        if os.name == "nt":
+            args = [sublime.packages_path() + '\LESS\windows\lessc.exe']
+        else:
+            css_path = opts['css_path']
+            less_path = opts['less_path']
+            args = ['lessc', '-x', less_path + 'style.less', css_path + 'style.css']
+            subprocess.call(args)
+            args = ['java', '-jar', opts['cssembed_path'], css_path + 'style.css', '-o', css_path + 'style_base64.css']
+            subprocess.call(args)
+            os.remove(css_path + 'style.css')
+            os.rename(css_path + 'style_base64.css', css_path + 'style.css')
+            view.set_status('less', 'File saved and LESS compiled for  ' + folder_name)
---
+++ lessc_opts.py
@@ -0,0 +1,14 @@
+lessc_opts = {
+    '/media/samba/devel.ulabox.com/htdocs/css/_less/': {
+        #css path
+        'css_path': '/media/samba/devel.ulabox.com/htdocs/css/',
+        #path for cssembed
+        'cssembed_path': '/media/samba/lib/cssembed-0.4.5.jar',
+    },
+    '/media/samba/devel.ulabox.com/htdocs/static/less/': {
+        #css path
+        'css_path': '/media/samba/devel.ulabox.com/htdocs/static/css/',
+        #path for cssembed
+        'cssembed_path': '/media/samba/lib/cssembed-0.4.5.jar',
+    }
+}
